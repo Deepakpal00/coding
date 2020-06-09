a1752448f89e6aad747bec196f4cb60884a6d6d1
Vipin Kr <vipinkr@Vipins-MacBook-Air.local>
2020-06-09 15:52:11 +0530

    Email sending Demo usnig spring boot

---
+++ SimpleEmailSenderUsingSpringBoot/src/main/java/com/deepak/SimpleEmailSenderUsingSpringBoot/SimpleEmailSenderUsingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.deepak.SimpleEmailSenderUsingSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleEmailSenderUsingSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleEmailSenderUsingSpringBootApplication.class, args);
+	}
+
+}
