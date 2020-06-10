9549114d0a8126c84d96b9e1021eb5f3181a4ef4
Vipin Kr <vipinkr@Vipins-MacBook-Air.local>
2020-06-10 16:09:43 +0530

    quick Find Multiple Left Rotation

---
+++ InterviewQuestions/coding/src/coding/com/arrays/QuicklyFindMultipleLeftRotaion.java
@@ -0,0 +1,30 @@
+package coding.com.arrays;
+
+public class QuicklyFindMultipleLeftRotaion {
+
+	public static void main(String[] args) {
+		int array[] = { 1, 3, 5, 7, 9 }; // 5 7 9 1 3
+		int n = array.length;
+
+		int k = 2;
+		leftRotate(array, n, k);
+		System.out.println();
+
+		k = 3;
+		leftRotate(array, n, k);
+		System.out.println();
+
+		k = 4;
+		leftRotate(array, n, k);
+		System.out.println();
+	}
+
+	private static void leftRotate(int[] array, int n, int k) {
+		
+		for(int i = k; i < n+k;i++) {
+			System.out.print(array[i % n] + " ");
+		}
+
+	}
+
+}
